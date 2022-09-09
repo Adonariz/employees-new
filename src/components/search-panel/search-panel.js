3dedb5de13b1cb064e358a18905bbdac243d74ed
alexey.zhila <aleksey.zhila@kartoteka.ru>
2022-09-09 16:10:21 +0300

    разбивает верстку на компоненты

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,9 @@
+import './search-panel.css';
+
+const SearchPanel = () => {
+  return (
+    <input className="form-control search-input" type="text" placeholder="Найти сотрудника" />
+  );
+};
+
+export default SearchPanel;
