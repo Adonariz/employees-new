f107efc537f6fecce3a56f6ec31b38a9c2e8de54
alexey.zhila <aleksey.zhila@kartoteka.ru>
2022-09-09 17:40:58 +0300

    добавляет генерацию сотрудников через пропсы

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,6 +7,12 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 import './app.css';
 
 function App() {
+  const data = [
+    {name: 'John Smith', salary: 800, increase: true},
+    {name: 'Alex Shepard', salary: 3000, increase: false},
+    {name: 'Biggus Dickus', salary: 5000, increase: false},
+  ];
+
   return (
     <div className="app">
       <AppInfo />
@@ -16,7 +22,7 @@ function App() {
         <AppFilter />
       </div>
       
-      <EmployeesList />
+      <EmployeesList data={data} />
       <EmployeesAddForm />
     </div>
   );
